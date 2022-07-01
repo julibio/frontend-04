09908031e79fa17df367ff4bf2a603638ec25a91
Guilherme <guiialexandrino@gmail.com>
2022-07-01 14:36:02 -0300

    Alteracao

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,51 @@
 <template>
   <div style="padding-top: 2rem">
-    <button :style="paginaAtual == 'curso' ?'background-color: #000' : ''" @click="mudaComponente('curso')">Curso</button>
-    <button @click="mudaComponente('aluno')">Aluno</button>
-    <button @click="mudaComponente('diario')">Diário</button>
+    <button
+      :style="paginaAtual == 'curso' ? 'background-color: #000' : ''"
+      @click="mudaComponente('curso')"
+    >
+      Curso
+    </button>
+    <button
+      :style="paginaAtual == 'aluno' ? 'background-color: #000' : ''"
+      @click="mudaComponente('aluno')"
+    >
+      Aluno
+    </button>
+    <button
+      :style="paginaAtual == 'diario' ? 'background-color: #000' : ''"
+      @click="mudaComponente('diario')"
+    >
+      Diário
+    </button>
   </div>
-  <MainPage v-if="paginaAtual == 'aluno'"/>
-  <pagina-curso v-if="paginaAtual == 'curso'"/>
-  <pagina-diario v-if="paginaAtual == 'diario'"/>
+  <MainPage v-if="paginaAtual == 'aluno'" />
+  <pagina-curso v-if="paginaAtual == 'curso'" />
+  <pagina-diario v-if="paginaAtual == 'diario'" />
 </template>
 
 <script>
-import MainPage from "./views/MainPage.vue"
-import PaginaCurso from "./views/Curso.vue"
-import PaginaDiario from "./views/Diario.vue"
-import { ref } from '@vue/reactivity'
+import MainPage from './views/MainPage.vue';
+import PaginaCurso from './views/Curso.vue';
+import PaginaDiario from './views/Diario.vue';
+import { ref } from '@vue/reactivity';
 
 export default {
   name: 'App',
   components: {
     MainPage,
     PaginaCurso,
-    PaginaDiario
+    PaginaDiario,
   },
-  setup(){
-    const paginaAtual = ref("aluno")
-    function mudaComponente(componente){
-      paginaAtual.value = componente
+  setup() {
+    const paginaAtual = ref('aluno');
+    function mudaComponente(componente) {
+      paginaAtual.value = componente;
     }
 
-    return {paginaAtual, mudaComponente}
-  }
-}
+    return { paginaAtual, mudaComponente };
+  },
+};
 </script>
 
-<style src="./assets/style.css"/>
-
+<style src="./assets/style.css" />
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -7,28 +7,38 @@
       </div>
       <div>
         <form>
-          <input v-model="formData.name" placeholder="Student Name"/>
-          <input v-model="formData.course" placeholder="Course"/>
-          <input v-model="formData.rating" placeholder="Rating"/>
-          <button 
-          v-if="!editMode"
-          @click.prevent="addStudent()"
-          :disabled="!formData.name || !formData.course || !formData.rating"
-          >Add</button>
+          <input v-model="formData.name" placeholder="Student Name" />
+          <input v-model="formData.course" placeholder="Course" />
+          <input v-model="formData.rating" placeholder="Rating" />
           <button
-          v-if="!editMode"
-          @click.prevent="reset"
-          :disabled="!formData.name && !formData.course && !formData.rating"
-          class="resetButton"
+            v-if="!editMode"
+            @click.prevent="addStudent()"
+            :disabled="!formData.name"
           >
-          Reset
+            Add
           </button>
-          <button v-if="editMode" @click.prevent="saveModifiedStudent">Save</button>
-          <button v-if="editMode" class="resetButton" @click.prevent="cancel">Cancel</button>
+          <button
+            v-if="!editMode"
+            @click.prevent="reset"
+            :disabled="!formData.name"
+            class="resetButton"
+          >
+            Reset
+          </button>
+          <button v-if="editMode" @click.prevent="saveModifiedStudent">
+            Save
+          </button>
+          <button v-if="editMode" class="resetButton" @click.prevent="cancel">
+            Cancel
+          </button>
         </form>
       </div>
       <div>
-        <TableComponent :dataset="dataset" @tableEditClick="editStudent" @tableRemoveClick="deleteStudent"/>
+        <TableComponent
+          :dataset="dataset"
+          @tableEditClick="editStudent"
+          @tableRemoveClick="deleteStudent"
+        />
       </div>
     </section>
   </div>
@@ -37,87 +47,91 @@
 <script>
 import { ref } from '@vue/reactivity';
 import TableComponent from '../components/TableComponent.vue';
-import Services from "../services/services"
+import Services from '../services/services';
 
 export default {
-  name: "MainPage",
+  name: 'MainPage',
   components: {
-    TableComponent
+    TableComponent,
   },
   setup() {
-    const dataset = ref()
+    const dataset = ref();
 
-    async function loadDataBase(){
-      try{
-        let req = await Services.getStudents()
-        if (req){
-          dataset.value = req
-          cancel()
+    async function loadDataBase() {
+      try {
+        let req = await Services.getStudents();
+        if (req) {
+          dataset.value = req;
+          cancel();
         }
+      } catch (error) {
+        console.log(error);
       }
-      catch(error){
-        console.log(error)
-      }
     }
 
-    loadDataBase()
+    loadDataBase();
 
-    const formData = ref({id: "", name: "", course: "", rating: ""})
-    const editMode = ref(false)
+    const formData = ref({ id: '', name: '', course: '', rating: '' });
+    const editMode = ref(false);
 
-    function reset(){
-      formData.value.name = ""
-      formData.value.course = ""
-      formData.value.rating = ""
-      formData.value.id = ""
+    function reset() {
+      formData.value.name = '';
+      formData.value.course = '';
+      formData.value.rating = '';
+      formData.value.id = '';
     }
 
-    function cancel(){
-      reset()
-      editMode.value = false
+    function cancel() {
+      reset();
+      editMode.value = false;
     }
 
-    async function addStudent(){
-      console.log("clicou para adicionar")
-      try{
-        let req = await Services.addStudent(formData.value.name,formData.value.course,formData.value.rating)
-        if(req){
-          await loadDataBase()
-          editMode.value = false
+    async function addStudent() {
+      console.log('clicou para adicionar');
+      try {
+        let req = await Services.addStudent(
+          formData.value.name,
+          formData.value.course,
+          formData.value.rating,
+        );
+        if (req) {
+          await loadDataBase();
+          editMode.value = false;
         }
-      }catch(error){console.log(error)}
+      } catch (error) {
+        console.log(error);
+      }
     }
 
-    function editStudent(student){
-      editMode.value = true
-      formData.value.name = student.name
-      formData.value.course = student.course
-      formData.value.rating = student.rating
-      formData.value.id = student.id
+    function editStudent(student) {
+      editMode.value = true;
+      formData.value.name = student.name;
+      formData.value.course = student.course;
+      formData.value.rating = student.rating;
+      formData.value.id = student.id;
     }
 
-    async function saveModifiedStudent(){
-      try{
+    async function saveModifiedStudent() {
+      try {
         let req = await Services.editStudent(
           formData.value.id,
           formData.value.name,
           formData.value.course,
-          formData.value.rating
-        )
-        if(req) await loadDataBase()
-
-      }catch(error){
-        console.log(error)
+          formData.value.rating,
+        );
+        if (req) await loadDataBase();
+      } catch (error) {
+        console.log(error);
       }
     }
 
-    async function deleteStudent(student){
-      try{
-        await Services.deleteStudent(student.id)
-      }catch(error){
-        console.log(error)
+    async function deleteStudent(student) {
+      try {
+        await Services.deleteStudent(student.id);
+      } catch (error) {
+        console.log(error);
       }
-      await loadDataBase()
+      await loadDataBase();
     }
 
     return {
@@ -129,11 +143,10 @@ export default {
       addStudent,
       editStudent,
       saveModifiedStudent,
-      deleteStudent
-    }
+      deleteStudent,
+    };
   },
 };
 </script>
 
-<style>
-</style>
+<style></style>
